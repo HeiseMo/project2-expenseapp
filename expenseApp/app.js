badb9000d1b4e9693c8f94c53943410d679bdf72
Nate <mcfwesh@gmail.com>
2020-04-27 17:15:02 +0200

    items model started

--- expenseApp/app.js
+++ expenseApp/app.js
@@ -1,56 +1,64 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
-const bcrypt = require('bcrypt');
-const flash = require('connect-flash');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const flash = require("connect-flash");
 
 //Session Stuff
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 // require passport and the local strategy
-const ensureLogin = require('connect-ensure-login');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const ensureLogin = require("connect-ensure-login");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 // User model
 const User = require("./models/User");
 
 mongoose
-  .connect('mongodb://localhost/expenseapp', {
-    useNewUrlParser: true
+  .connect("mongodb://localhost/expenseapp", {
+    useNewUrlParser: true,
   })
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
-app.use(session({
-  secret: "our-passport-local-strategy-app",
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 //Passport Middleware
 
@@ -69,58 +77,61 @@ passport.deserializeUser((id, cb) => {
 
 app.use(flash());
 
-passport.use(new LocalStrategy((username, password, next) => {
-  User.findOne({
-    username
-  }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, {
-        message: "Incorrect credentials"
-      });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, {
-        message: "Incorrect credentials"
-      });
-    }
+passport.use(
+  new LocalStrategy((username, password, next) => {
+    User.findOne(
+      {
+        username,
+      },
+      (err, user) => {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return next(null, false, {
+            message: "Incorrect credentials",
+          });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return next(null, false, {
+            message: "Incorrect credentials",
+          });
+        }
 
-    return next(null, user);
-  });
-}));
+        return next(null, user);
+      }
+    );
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true,
+  })
+);
 
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
+const index = require("./routes/index");
+app.use("/", index);
 
+const authRoutes = require("./routes/auth-routes");
+app.use("/", authRoutes);
 
-const index = require('./routes/index');
-app.use('/', index);
-
-const authRoutes = require('./routes/auth-routes');
-app.use('/', authRoutes);
-
+const itemRoutes = require("./routes/item-routes");
+app.use("/", itemRoutes);
 
 module.exports = app;
