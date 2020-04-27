e5f3f4c71b6eee9547ab6ea70c24526b0a9bf13c
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-27 12:05:59 +0200

    Login/Signup Done

---
+++ expenseApp/app.js
@@ -0,0 +1,126 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const flash = require('connect-flash');
+
+//Session Stuff
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+// require passport and the local strategy
+const ensureLogin = require('connect-ensure-login');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+// User model
+const User = require("./models/User");
+
+mongoose
+  .connect('mongodb://localhost/expenseapp', {
+    useNewUrlParser: true
+  })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Passport Middleware
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+app.use(flash());
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, {
+        message: "Incorrect credentials"
+      });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, {
+        message: "Incorrect credentials"
+      });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+
+const index = require('./routes/index');
+app.use('/', index);
+
+const authRoutes = require('./routes/auth-routes');
+app.use('/', authRoutes);
+
+
+module.exports = app;
---
+++ expenseApp/routes/auth-routes.js
@@ -0,0 +1,98 @@
+// routes/auth-routes.js
+const express = require("express");
+const router = express.Router();
+const flash = require('connect-flash');
+
+// User model
+const User = require("../models/User");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+// Passport
+
+const passport = require('passport')
+
+const ensureLogin = require('connect-ensure-login');
+
+
+
+router.get("/", (req, res, next) => {
+    res.render("auth/index");
+});
+
+
+//Signup Stuff 
+
+router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+});
+
+router.post('/signup', (req, res, next) => {
+    const {
+        username,
+        password
+    } = req.body;
+    if (password.length < 8) {
+        res.render('auth/signup', {
+            message: 'Your password must be 8 characters minimum'
+        });
+        return;
+    }
+    if (username === '') {
+        res.render('auth/signup', {
+            message: 'Your username cannot be empty'
+        });
+        return;
+    }
+
+    User.findOne({
+        username: username
+    }).then(found => {
+        if (found !== null) {
+            res.render('signup', {
+                message: 'Username already exists!'
+            });
+        } else {
+            const salt = bcrypt.genSaltSync();
+            const hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+                    username: username,
+                    password: hash
+                })
+                .then(userDB => {
+                    res.redirect('auth/login');
+                })
+                .catch(err => {
+                    next(err);
+                });
+        }
+    })
+});
+
+//Login Stuff
+router.get('/login', (req, res, next) => {
+    res.render('auth/login', { message: req.flash('error') });
+  });
+
+router.post('/login',
+    passport.authenticate('local', {
+        failureRedirect: '/login',
+        failureFlash: true,
+        passReqToCallback: true,
+    }),
+    function (req, res) {
+        res.redirect('/private-page');
+    });
+
+//VIP AREA
+
+router.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res) => {
+    res.render('auth/private', {
+        user: req.user
+    });
+});
+
+module.exports = router;
