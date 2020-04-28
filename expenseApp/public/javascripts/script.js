0cd70866e3df4788ae1e2da98b686dd952cf49b6
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-28 17:49:00 +0200

    Login/signup design doneish

--- expenseApp/public/javascripts/script.js
+++ expenseApp/public/javascripts/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+    console.log('IronGenerator JS imported successfully!');
 
 }, false);
 
