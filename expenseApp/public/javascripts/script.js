0c6b992c662649bec720f4fdf630b9d82585dab1
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-30 18:13:50 +0200

    more styling

--- expenseApp/public/javascripts/script.js
+++ expenseApp/public/javascripts/script.js
@@ -5,4 +5,17 @@ document.addEventListener(
   },
   false
 );
-
+const languages = {
+  en: {
+    overlayLogin: "Login",
+    usernamePlaceholder: "Username",
+    passwordPlaceholder: "Password",
+    dashboardAddExpenseH1: "Add Expense"
+  },
+  tr: {
+    overlayLogin: "Giris",
+    usernamePlaceholder: "Kullanici Adi",
+    passwordPlaceholder: "Sifre",
+    dashboardAddExpenseH1: "Gider Ekle"
+  }
+};
