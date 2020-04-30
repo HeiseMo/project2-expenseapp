90480277d5873ed7612541a7d2dab6993f873055
HeiseMo <tim.james.tanurhan@gmail.com>
2020-04-30 23:36:22 +0200

    Reports page finished, edit modal finished

--- expenseApp/public/javascripts/dashboard.js
+++ expenseApp/public/javascripts/dashboard.js
@@ -1,20 +1,31 @@
 var modal = document.querySelector(".modal");
+var modalEdit = document.querySelector(".modalEdit");
 var trigger = document.querySelector(".addExpenseBtn");
 var closeButton = document.querySelector(".close-button");
+var triggerEdit = document.querySelector(".editExpenseBtn");
 
 function toggleModal() {
   modal.classList.toggle("show-modal");
+}
+
+function toggleEditModal() {
+  modalEdit.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
   if (event.target === modal) {
     toggleModal();
   }
+  if (event.target === modalEdit){
+    console.log("edit modal");
+    toggleEditModal();
+  }
 }
-
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+triggerEdit.addEventListener("click", toggleEditModal);
+
 
 let expensePrice = document.querySelectorAll(".expenseSum");
 console.log(expensePrice[3]);
